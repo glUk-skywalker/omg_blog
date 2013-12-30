131f5349d8472a0eeea91260e3ce93cec450e2b1
glUk <glUk.main+github@gmail.com>
2013-12-30 23:09:26 +0400

    initial commit. rails default

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OmgBlog::Application.config.secret_key_base = 'e7bfdef8b687e71e7ea0a63c02c6134b7bd74b56582d42ce539b53a37cb359b476fe1bc74fffa9095e39cba95c72036052fa605ace5a85fb6f531e78e0cc6763'
