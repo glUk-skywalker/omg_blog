7b82ea07196114ab82da890f132ac4d092aba498
glUk <glUk.main+github@gmail.com>
2013-12-31 00:06:12 +0400

    articles added as resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 OmgBlog::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  resources :articles
+
   root to: 'articles#index'
 
   # You can have the root of your site routed with "root"
