cc746e2ab82245d5ea2b74a0f3034a3fe95acaac
glUk <glUk.main+github@gmail.com>
2014-01-01 19:17:01 +0400

    owner_id validate is replaced with user_id, since corresponding column is changed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-  validates :owner_id, presence: true
+  validates :user_id, presence: true
   validates :title, presence: true
   validates :text, presence: true, length: { minimum: 30 }
 
