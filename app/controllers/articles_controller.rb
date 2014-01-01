f1a8cf32e73a86f4821a531fb47f378584fc86a3
glUk <glUk.main+github@gmail.com>
2014-01-01 19:18:18 +0400

    added new article creation authentication is now needed only for all actions, except index and show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_filter :authenticate_user!, only: [:new, :edit]
+  before_filter :authenticate_user!, except: [:index, :show]
 
   def index
     @articles = Article.all
@@ -13,6 +13,20 @@ class ArticlesController < ApplicationController
     rescue ActiveRecord::RecordNotFound => e
       flash[:notice] = 'No such article'
       redirect_to root_path
+    end
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(user_id: current_user.id, title: params[:article][:title], text: params[:article][:text])
+    if @article.save
+      redirect_to @article
+    else
+      flash.now[:notice] = @article.errors.full_messages.to_s
+      render 'new'
     end
   end
 end
